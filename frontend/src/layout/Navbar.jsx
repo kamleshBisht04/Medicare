626186011cf3e6989ab0d16aa8c0e2c5b3ae4c1e
kamleshBisht04 <kamleshbisht04@gmail.com>
2026-05-08 23:48:02 +0530

    structure of navbar design for tailwind class

--- frontend/src/layout/Navbar.jsx
+++ frontend/src/layout/Navbar.jsx
@@ -1,5 +1,44 @@
+import { assets } from '@/assets/assets';
+import { NavLink } from 'react-router-dom';
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="mb-5 flex items-center justify-between border-b border-b-gray-400/30 py-2">
+      <img
+        src={assets.logo}
+        alt="medicare logo "
+        className="h-10 md:h-14  cursor-pointer"
+      />
+      <ul className="hidden items-center font-medium gap-8 md:flex">
+        <NavLink>
+          <li>
+            HOME
+            <hr />
+          </li>
+        </NavLink>
+        <NavLink>
+          <li>
+            ALL DOCTORS
+            <hr />
+          </li>
+        </NavLink>
+        <NavLink>
+          <li>
+            ABOUT
+            <hr />
+          </li>
+        </NavLink>
+        <NavLink>
+          <li>
+            CONTACTS
+            <hr />
+          </li>
+        </NavLink>
+      </ul>
+      <div>
+        <button>Create account</button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
