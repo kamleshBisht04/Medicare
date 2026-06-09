10697bcd3f03bf44b346e7fe03e0b4865c069407
kamleshBisht04 <kamleshbisht04@gmail.com>
2026-06-09 21:31:34 +0530

    setting the backend and install all package and setting the paclage .json

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,18 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-import connectDB from "./configs/mongodb.js";
-import connectCloudinary from "./configs/cloudinary.js";
 
 // api config
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
-connectDB();
-connectCloudinary();
-
-// Middlewers
-app.use(express.json());
+// Middleware
 app.use(cors());
+app.use(express.json());
 
-// api endPoints
+// Test Rout
 app.get("/", (req, res) => {
-  res.send("API IS WORKING");
+  res.send("API IS WORKING..");
 });
 
-app.listen(port, () => console.log(`Server started ${port}`));
+app.listen(port, () => console.log(`server started at port no ${port}`));
