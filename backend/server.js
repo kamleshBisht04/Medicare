2ca75ca32ec61f796c04aa4bead9f33a09ffb690
kamleshBisht04 <kamleshbisht04@gmail.com>
2026-06-09 23:23:23 +0530

    connect the cloudinary.js for photo uploade

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,15 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./configs/mongodb.js";
-
+import connectCloudinary from "./configs/cloudinary.js";
 
 // api config
 const app = express();
 const port = process.env.PORT || 4000;
+
+// Database Connection & Cloudinary Connection
 connectDB();
+connectCloudinary();
 
 // Middleware
 app.use(cors());
@@ -15,7 +18,7 @@ app.use(express.json());
 
 // Test Rout
 app.get("/", (req, res) => {
-  res.send("API IS WORKING..");
+  res.send("API is working...");
 });
 
 app.listen(port, () => console.log(`server started at port no ${port}`));
