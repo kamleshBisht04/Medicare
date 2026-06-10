46840c9aba701aff69929342a8a275f37fd29313
kamleshBisht04 <kamleshbisht04@gmail.com>
2026-06-10 15:45:30 +0530

    creating adminlogin routes and test

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -2,6 +2,7 @@ import validator from "validator";
 import bcrypt from "bcrypt";
 import { v2 as cloudinary } from "cloudinary";
 import doctorModel from "../models/doctorModel.js";
+import jwt from "jsonwebtoken";
 
 // Add Doctor API
 const addDoctor = async (req, res) => {
@@ -26,7 +27,7 @@ const addDoctor = async (req, res) => {
       !address ||
       !imageFile
     ) {
-      return res.json({
+      return res.status(400).json({
         success: false,
         message: "Missing Details",
       });
@@ -34,14 +35,14 @@ const addDoctor = async (req, res) => {
 
     // Vaildator email
     if (!validator.isEmail(email)) {
-      return res.json({
+      return res.status(400).json({
         success: false,
         message: "Please enter a vaild email",
       });
     }
     // Validate password length
     if (password.length < 8) {
-      return res.json({
+      return res.status(400).json({
         success: false,
         message: "Please enter a strong password",
       });
@@ -80,17 +81,44 @@ const addDoctor = async (req, res) => {
     const newDoctor = new doctorModel(doctorData);
     await newDoctor.save();
 
-    res.json({
+    return res.status(201).json({
       success: true,
       message: "Doctor Added Successfully",
     });
   } catch (error) {
     console.log(error);
-    res.json({
+    return res.status(500).json({
       success: false,
       message: error.message,
     });
   }
 };
 
-export { addDoctor };
+// API  FOR ADMIN LOGIN
+
+const loginAdmin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
+      const token = jwt.sign({ email, role: "admin" }, process.env.JWT_SECRET, { expiresIn: "1d" });
+
+      return res.status(200).json({
+        success: true,
+        token,
+      });
+    } else {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export { addDoctor, loginAdmin };
--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
 import upload from "../middleware/multer.js";
-import { addDoctor } from "../controllers/adminController.js";
+import { addDoctor, loginAdmin } from "../controllers/adminController.js";
 
 const adminRouter = express.Router();
 
 adminRouter.post("/add-doctor", upload.single("image"), addDoctor);
+adminRouter.post("/login", loginAdmin);
 
 export default adminRouter;
