3c77cb9906e369fd59eb37e79e2460a7a416757e
kamlesh bisht <kamleshbisht04@gmail.com>
2026-08-24 20:07:49 +0530

    navbar and structure ready

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,20 +8,23 @@ import MyProfile from "@/pages/MyProfile";
 import MyAppointments from "@/pages/MyAppointments";
 import Navbar from "@/layout/Navbar";
 import Footer from "@/layout/Footer";
+import Appointment from "@/pages/Appointment";
 
 const App = () => {
   return (
     <>
       <Navbar />
-      <div>
+      <div className="mx-4 sm:mx-[10%]">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/doctors" element={<Doctors />} />
+          <Route path="/doctors/:speciality" element={<Doctors />} />
           <Route path="/login" element={<Login />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/my-profile" element={<MyProfile />} />
           <Route path="/my-appointments" element={<MyAppointments />} />
+          <Route path="/appointment/:docId" element={<Appointment />} />
         </Routes>
       </div>
       <Footer />
---
+++ frontend/src/context/AppContext.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable react-refresh/only-export-components */
+
+import { createContext, useContext } from "react";
+import { doctors } from "@/assets/assets";
+export const AppContext = createContext();
+
+// provider
+
+export const AppContextProvider = ({ children }) => {
+  const value = {doctors};
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
+
+// custome hook
+
+export const useAppContext = () => {
+  const context = useContext(AppContext);
+  if (!context) {
+    throw new Error("userAppContext must be used within AppProvider");
+  }
+  return context;
+};
+
+export default AppContextProvider;
