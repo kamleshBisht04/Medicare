3e53faeaf802b7ee746bf1e57aea715720004cc4
kamlesh bisht <kamleshbisht04@gmail.com>
2026-08-24 10:34:25 +0530

    medicare project start again

--- frontend/src/data/NavLinks.js
+++
@@ -1,6 +0,0 @@
-export const NavLinks = [
-  { name: 'HOME', path: '/' },
-  { name: 'ALL DOCTORS', path: '/doctors' },
-  { name: 'ABOUT', path: '/about' },
-  { name: 'CONTACT', path: '/contact' },
-];
