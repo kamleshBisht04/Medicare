ce020dd65c4a4b7d42a2153b4b49ccc0bb160b33
kamlesh bisht <kamleshbisht04@gmail.com>
2026-08-25 22:22:51 +0530

    specialityMenu and topdoctors and bottom banner redesign again

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,17 @@
+import BottomBanner from "../components/BottomBanner";
+import Header from "../components/Header";
+import SpecialityMenu from "../components/SpecialityMenu";
+import TopDoctors from "../components/TopDoctors";
+
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <SpecialityMenu />
+      <TopDoctors />
+      <BottomBanner />
+    </div>
+  );
 };
 
 export default Home;
