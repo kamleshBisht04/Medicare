c4b84ac06a2a3324740f531b001e98b6b0cd1fae
kamlesh bisht <kamleshbisht04@gmail.com>
2026-08-29 14:09:04 +0530

    doctorModel created

---
+++ backend/models/doctorModel.js
@@ -0,0 +1,92 @@
+import mongoose from "mongoose";
+
+const doctorSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    speciality: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    degree: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    experience: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    about: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    fees: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    address: {
+      street: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+
+      city: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+
+      state: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+
+      pincode: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+    },
+    available: {
+      type: Boolean,
+      default: true,
+    },
+    slots_booked: {
+      type: Object,
+      default: {},
+    },
+  },
+  {
+    minimize: false,
+    timestamps: true,
+  },
+);
+
+const doctorModel = mongoose.models.doctor || mongoose.model("doctor", doctorSchema);
+
+export default doctorModel;
