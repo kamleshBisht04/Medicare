a71ad847b878e8f77a6e06c7783e3ba12ab5ebce
kamlesh bisht <kamleshbisht04@gmail.com>
2026-08-30 21:09:43 +0530

    addDoctor api route added

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,2 +1,98 @@
-// API for adding doctor
+import validator from "validator";
+import bcrypt from "bcrypt";
+import doctorModel from "../models/doctorModel.js";
+import { v2 as cloudinary } from "cloudinary";
 
+const addDoctor = async (req, res) => {
+  try {
+    const { name, email, password, experience, fees, speciality, degree, about, street, city, state, pincode, } = req.body;
+
+    const imageFile = req.file;
+
+    // Check required fields
+    if ( !name || !email || !password || !experience || !fees || !speciality || !degree || !about || !street || !city || !state || !pincode || !imageFile ) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
+    }
+
+    // Validate email
+    if (!validator.isEmail(email)) {
+      return res.status(400).json({
+        success: false,
+        message: "Please enter a valid email",
+      });
+    }
+
+    // Validate password
+    if (password.length < 8) {
+      return res.status(400).json({
+        success: false,
+        message: "Password must be at least 8 characters",
+      });
+    }
+
+    // Check if doctor already exists
+    const existingDoctor = await doctorModel.findOne({ email });
+
+    if (existingDoctor) {
+      return res.status(409).json({
+        success: false,
+        message: "Doctor with this email already exists",
+      });
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Upload image to Cloudinary
+
+    const imageUpload = await cloudinary.uploader.upload(imageFile.path, {
+      resource_type: "image",
+    });
+    const imageUrl = imageUpload.secure_url;
+
+    // Create doctor data
+    const doctorData = {
+      name,
+      email,
+      password: hashedPassword,
+      image: imageUrl,
+      experience,
+      fees,
+      speciality,
+      degree,
+      about,
+
+      address: {
+        street,
+        city,
+        state,
+        pincode,
+      },
+
+      date: Date.now(),
+    };
+
+    // Save doctor
+    const doctor = new doctorModel(doctorData);
+
+    await doctor.save();
+
+    return res.status(201).json({
+      success: true,
+      message: "Doctor added successfully",
+    });
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export { addDoctor };
