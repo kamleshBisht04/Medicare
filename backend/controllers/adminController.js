6e34b40880aa0371fd510373055569a2e32e0e41
kamlesh bisht <kamleshbisht04@gmail.com>
2026-09-05 13:23:58 +0530

    changeAvailablity backend routes update

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -164,4 +164,26 @@ const getAllDoctors = async (req, res) => {
   }
 };
 
-export { addDoctor, loginAdmin, getAllDoctors };
+// API FOR CHANGEING AVAILABILITY OF DOCTOR
+
+const changeAvailablity = async (req, res) => {
+  try {
+    const { id, available } = req.body;
+
+    await doctorModel.findByIdAndUpdate(id, {
+      available,
+    });
+    res.status(200).json({
+      success: true,
+      message: "Doctor availability updated",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export { addDoctor, loginAdmin, getAllDoctors, changeAvailablity };
--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 import upload from "../middlewares/multer.js";
-import { addDoctor, getAllDoctors, loginAdmin } from "../controllers/adminController.js";
+import { addDoctor, changeAvailablity, getAllDoctors, loginAdmin, } from "../controllers/adminController.js";
 import authAdmin from "../middlewares/authAdmin.js";
 
 const adminRouter = express.Router();
@@ -8,5 +8,6 @@ const adminRouter = express.Router();
 adminRouter.post("/add-doctor", authAdmin, upload.single("image"), addDoctor);
 adminRouter.post("/login", loginAdmin);
 adminRouter.get("/doctor-list", authAdmin, getAllDoctors);
+adminRouter.post("/change-availability", authAdmin, changeAvailablity);
 
 export default adminRouter;
