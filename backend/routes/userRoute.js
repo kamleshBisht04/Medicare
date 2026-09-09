ba9bb22f6348a162dc71a78ef4da16f8999aa5c1
kamlesh bisht <kamleshbisht04@gmail.com>
2026-09-09 23:14:47 +0530

    correct the userRout '

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,12 +1,6 @@
 import express from "express";
 import upload from "../middlewares/multer.js";
-import {
-  bookAppointment,
-  getProfile,
-  loginUser,
-  registerUser,
-  updateProfile,
-} from "../controllers/userController.js";
+import { bookAppointment, getAppointments, getProfile, loginUser, registerUser, updateProfile, } from "../controllers/userController.js";
 import authUser from "../middlewares/authUser.js";
 
 const userRouter = express.Router();
@@ -16,5 +10,6 @@ userRouter.post("/login", loginUser);
 userRouter.get("/get-profile", authUser, getProfile);
 userRouter.put("/update-profile", upload.single("image"), authUser, updateProfile);
 userRouter.post("/book-appointment", authUser, bookAppointment);
+userRouter.get("/get-appointments", authUser, getAppointments);
 
 export default userRouter;
