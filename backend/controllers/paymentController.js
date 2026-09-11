1d304e36d7a7a60220b557e0e5502be37870d9ad
kamlesh bisht <kamleshbisht04@gmail.com>
2026-09-11 10:27:27 +0530

    starting the interegtation of online payment

---
+++ backend/controllers/paymentController.js
@@ -0,0 +1,23 @@
+import Razorpay from "razorpay";
+
+//API to make payment of appointment using razorpay
+
+const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
+
+const paymentRazorpay = async (req, res) => {
+  try {
+
+    
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export { paymentRazorpay };
