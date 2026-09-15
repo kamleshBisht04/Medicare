f65fce1ced91ffb77fb9159879e30ed092538e11
kamlesh bisht <kamleshbisht04@gmail.com>
2026-09-15 16:20:45 +0530

    doctorAppoiment are display in doctor dashboard

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -1,5 +1,7 @@
 /* eslint-disable react-refresh/only-export-components */
+import axios from "axios";
 import { createContext, useState } from "react";
+import { toast } from "react-toastify";
 
 export const DoctorContext = createContext();
 
@@ -9,11 +11,36 @@ const DoctorContextProvider = ({ children }) => {
   const [dToken, setDToken] = useState(
     localStorage.getItem("dToken") ? localStorage.getItem("dToken") : "",
   );
+  const [appointments, setAppointments] = useState([])
 
+
+  const getDoctorAppointments = async () => {
+    try {
+      const { data } = await axios.get(
+        `${backendUrl}/api/doctor/all-appointments`,
+        {
+          headers: {
+            dToken,
+          },
+        },
+      );
+
+      if (data.success) {
+        setAppointments(data.appointments);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
   const value = {
     dToken,
     setDToken,
     backendUrl,
+    appointments,
+    setAppointments,
+    getDoctorAppointments
   };
 
   return (
